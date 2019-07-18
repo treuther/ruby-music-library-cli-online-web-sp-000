6403f615471b4ead86c630b7f4315c18c22c9590
Tracy Reuther <tracyreuther@gmail.com>
2019-07-18 09:42:21 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+class Song
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name, artist = nil)
+    @name = name
+    @artist = artist
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    song = new(name)
+    song.save
+    song
+  end
+
+end
