a099f195f1ac2004f56a806bca29b754ceed44c3
Tracy Reuther <tracyreuther@gmail.com>
2019-07-20 11:01:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,7 +51,7 @@ class Song
 
   def self.new_from_filename(name)
     artist, song, genre_name = name.split(" - ")
-    fixed_name = genre_name.gsub('.mp3', '')
+    fixed_name = genre_name.gsub(".mp3", "")
     artist = Artist.find_or_create_by_name(artist)
     artist = Genre.find_or_create_by_name(fixed_name)
     new(song, artist, genre)
