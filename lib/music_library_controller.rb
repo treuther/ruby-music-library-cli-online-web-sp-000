f35407cf1457bc487159d4623172e8294a9c1847
Tracy Reuther <tracyreuther@gmail.com>
2019-07-20 11:45:02 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -23,8 +23,8 @@ def call
 end
 
 def list_songs
-  Song.all.sort_by(&:name).each.with_index(1) do
-
+  Song.all.sort_by(&:name).each.with_index(1) do |song, idx|
+    puts "#{idx}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
   end
   binding.pry
 end
