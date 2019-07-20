f38c57a0fd9d975f23e59a28af92528423e94468
Tracy Reuther <tracyreuther@gmail.com>
2019-07-20 11:32:15 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -7,6 +7,9 @@ end
 def call
   puts "Welcome to your music library!"
   puts "To list all of your songs, enter 'list songs'."
+  puts "To list all of the artists in your library, enter 'list artists'."
+  puts "To list all of the genres in your library, enter 'list genres'."
+  
 end
 
 end
